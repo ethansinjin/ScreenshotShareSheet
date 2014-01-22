013422138609c17b9786f5cec3a2063613456e56
Ethan Gill <ethan.gill@me.com>
2014-01-22 12:10:06 -0600

    BigBoss version 1.0
    Release Candidate

--- ScreenshotShareSheet/ScreenshotShareSheet.mm
+++ ScreenshotShareSheet/ScreenshotShareSheet.mm
@@ -1,6 +1,5 @@
 #line 1 "/Users/ethangillius/Developer/Cydia/ScreenshotShareSheet/ScreenshotShareSheet/ScreenshotShareSheet.xm"
 #import <SpringBoard/SpringBoard.h>
-#import <BackBoardServices/BKSApplicationStateMonitor.h>
 #import "TTOpenInAppActivity.h"
 #import <AudioToolbox/AudioToolbox.h>
 
@@ -16,10 +15,10 @@
 
 #include <logos/logos.h>
 #include <substrate.h>
-@class SBScreenShotter; @class BKSApplicationStateMonitor; 
-static void (*_logos_orig$_ungrouped$SBScreenShotter$finishedWritingScreenshot$didFinishSavingWithError$context$)(SBScreenShotter*, SEL, id, id, void*); static void _logos_method$_ungrouped$SBScreenShotter$finishedWritingScreenshot$didFinishSavingWithError$context$(SBScreenShotter*, SEL, id, id, void*); static id (*_logos_orig$_ungrouped$BKSApplicationStateMonitor$bundleInfoValueForKey$PID$)(BKSApplicationStateMonitor*, SEL, id, int); static id _logos_method$_ungrouped$BKSApplicationStateMonitor$bundleInfoValueForKey$PID$(BKSApplicationStateMonitor*, SEL, id, int); 
+@class SBScreenShotter; 
+static void (*_logos_orig$_ungrouped$SBScreenShotter$finishedWritingScreenshot$didFinishSavingWithError$context$)(SBScreenShotter*, SEL, id, id, void*); static void _logos_method$_ungrouped$SBScreenShotter$finishedWritingScreenshot$didFinishSavingWithError$context$(SBScreenShotter*, SEL, id, id, void*); 
 
-#line 16 "/Users/ethangillius/Developer/Cydia/ScreenshotShareSheet/ScreenshotShareSheet/ScreenshotShareSheet.xm"
+#line 15 "/Users/ethangillius/Developer/Cydia/ScreenshotShareSheet/ScreenshotShareSheet/ScreenshotShareSheet.xm"
 
 
 
@@ -28,102 +27,109 @@ static void _logos_method$_ungrouped$SBScreenShotter$finishedWritingScreenshot$d
     
     
     
-
     ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
     
     
-    [library enumerateGroupsWithTypes:ALAssetsGroupSavedPhotos usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
+    NSString *settingsPath = [NSString stringWithFormat:@"%@/Library/Preferences/%@", NSHomeDirectory(), @"com.ethansquared.ScreenshotShareSheet.plist"];
+    NSMutableDictionary *settings = [NSMutableDictionary dictionaryWithContentsOfFile:
+                     settingsPath];
+	NSNumber* enabledPreference = [settings objectForKey:@"enabled"];
+    NSNumber* openInPreference = [settings objectForKey:@"openinenabled"];
+    NSNumber* delayPreference = [settings objectForKey:@"delay"];
+    
+    if(settings == nil){
+        settings = [[NSMutableDictionary alloc] init];
+        enabledPreference = [NSNumber numberWithBool:YES];
+        openInPreference = [NSNumber numberWithBool:NO];
+        delayPreference = [NSNumber numberWithDouble:0.40];
+        [settings setObject:enabledPreference forKey:@"enabled"];
+        [settings setObject:openInPreference forKey:@"openinenabled"];
+        [settings setObject:delayPreference forKey:@"delay"];
+        BOOL result = [settings writeToFile:settingsPath atomically:YES];
+        if(result){
+            NSLog(@"The user didn't have a settings file yet. Generated one");
+        }else{
+            NSLog(@"The user didn't have a settings file yet. Generating FAILED");
+        }
+    }
+    
+    if([enabledPreference boolValue]){
+    
         
+        [library enumerateGroupsWithTypes:ALAssetsGroupSavedPhotos usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
         
-        [group setAssetsFilter:[ALAssetsFilter allPhotos]];
-        
-        
-        [group enumerateAssetsAtIndexes:[NSIndexSet indexSetWithIndex:[group numberOfAssets] - 1] options:0 usingBlock:^(ALAsset *alAsset, NSUInteger index, BOOL *innerStop) {
             
+            [group setAssetsFilter:[ALAssetsFilter allPhotos]];
+        
             
-            if (alAsset) {
-                UIWindow* topWindow = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-                topWindow.hidden = NO;
-                topWindow.windowLevel = UIWindowLevelAlert;
-                UIViewController *vc  = [[UIViewController alloc] init];
+            [group enumerateAssetsAtIndexes:[NSIndexSet indexSetWithIndex:[group numberOfAssets] - 1] options:0 usingBlock:^(ALAsset *alAsset, NSUInteger index, BOOL *innerStop) {
+            
                 
-                [topWindow setRootViewController:vc];
+                if (alAsset) {
+                    UIWindow* topWindow = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+                    topWindow.hidden = NO;
+                    topWindow.windowLevel = UIWindowLevelAlert;
+                    UIViewController *vc  = [[UIViewController alloc] init];
                 
+                    [topWindow setRootViewController:vc];
+                    
                 
-                ALAssetRepresentation *representation = [alAsset defaultRepresentation];
-                UIImage *latestPhoto = [UIImage imageWithCGImage:[representation fullScreenImage]];
+                    ALAssetRepresentation *representation = [alAsset defaultRepresentation];
+                    UIImage *latestPhoto = [UIImage imageWithCGImage:[representation fullScreenImage]];
                 
-                NSString *tempDir = NSTemporaryDirectory();
-                NSString *path = [tempDir stringByAppendingPathComponent:[representation filename]];
-                NSURL *fileURL = [NSURL fileURLWithPath:path];
+                    NSString *tempDir = NSTemporaryDirectory();
+                    NSString *path = [tempDir stringByAppendingPathComponent:[representation filename]];
+                    NSURL *fileURL = [NSURL fileURLWithPath:path];
                 
-                [UIImagePNGRepresentation(latestPhoto) writeToFile:path atomically:NO];
+                    [UIImagePNGRepresentation(latestPhoto) writeToFile:path atomically:NO];
                 
-                TTOpenInAppActivity *openInActivity = [[TTOpenInAppActivity alloc] initWithView:vc.view andRect:vc.view.bounds];
+                    TTOpenInAppActivity *openInActivity = [[TTOpenInAppActivity alloc] initWithView:vc.view andRect:vc.view.bounds];
                 
+                    
+                    NSArray *activityItems = @[ fileURL ];
+                    UIActivityViewController *activityController;
+                    if([openInPreference boolValue]){
+                        activityController = [[UIActivityViewController alloc] initWithActivityItems:activityItems applicationActivities:@[ openInActivity ]];
+                    }else{
+                        activityController = [[UIActivityViewController alloc] initWithActivityItems:activityItems applicationActivities:@[ ]];
+                    }
+                    activityController.excludedActivityTypes = @[UIActivityTypeSaveToCameraRoll, UIActivityTypePrint, UIActivityTypeAirDrop ];
                 
-                NSArray *activityItems = @[ fileURL ];
-                UIActivityViewController *activityController =
-                [[UIActivityViewController alloc]
-                 initWithActivityItems:activityItems
-                 applicationActivities:@[ openInActivity ]];
-                activityController.excludedActivityTypes = @[UIActivityTypeSaveToCameraRoll,UIActivityTypeAssignToContact, UIActivityTypePrint, UIActivityTypeAirDrop ];
-                
-                if(UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone){
-                    openInActivity.superViewController = activityController;
-                    [openInActivity release];
-                    [vc presentViewController:activityController animated:YES completion:NULL];
-                } else {
-                    UIPopoverController *activityPopoverController = [[UIPopoverController alloc] initWithContentViewController:activityController];
-                    openInActivity.superViewController = activityPopoverController;
-                    [openInActivity release];
-                    [activityPopoverController presentPopoverFromRect:CGRectMake(CGRectGetMidX(vc.view.bounds), 0, 1, 1) inView:vc.view permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
-                 }
+                    if(UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone){
+                        openInActivity.superViewController = activityController;
+                        [openInActivity release];
+                        [vc presentViewController:activityController animated:YES completion:NULL];
+                    } else {
+                        UIPopoverController *activityPopoverController = [[UIPopoverController alloc] initWithContentViewController:activityController];
+                        openInActivity.superViewController = activityPopoverController;
+                        [openInActivity release];
+                        [activityPopoverController presentPopoverFromRect:CGRectMake(CGRectGetMidX(vc.view.bounds), 0, 1, 1) inView:vc.view permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+                    }
                 
 
                 
-                activityController.completionHandler = ^(NSString *activityType, BOOL completed) {
+                    activityController.completionHandler = ^(NSString *activityType, BOOL completed) {
+                        
                     
+                        [[NSFileManager defaultManager] removeItemAtURL:fileURL error:NULL];
                     
-                    [[NSFileManager defaultManager] removeItemAtURL:fileURL error:NULL];
-                    
-                    [topWindow performSelector:@selector(setHidden:) withObject:self afterDelay:0.40f]; 
-                    [topWindow release];
-                    [activityController release];
-                };
+                        [topWindow performSelector:@selector(setHidden:) withObject:self afterDelay:[delayPreference floatValue]]; 
+                        [topWindow release];
+                        [activityController release];
+                    };
                 
-            }
+                }
+            }];
+        } failureBlock: ^(NSError *error) {
+            
+            
+            NSLog(@"No groups");
         }];
-    } failureBlock: ^(NSError *error) {
-        
-        
-        NSLog(@"No groups");
-    }];
     
-    [library release];
+        [library release];
     
+    }
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     _logos_orig$_ungrouped$SBScreenShotter$finishedWritingScreenshot$didFinishSavingWithError$context$(self, _cmd, screenshot,error,context);
     
@@ -161,24 +167,6 @@ static void _logos_method$_ungrouped$SBScreenShotter$finishedWritingScreenshot$d
 
 
 
-
-
-
-
-static id _logos_method$_ungrouped$BKSApplicationStateMonitor$bundleInfoValueForKey$PID$(BKSApplicationStateMonitor* self, SEL _cmd, id arg1, int arg2) {
-    NSLog(@"-[<BKSApplicationStateMonitor: %p> bundleInfoValueForKey:%@ PID:%d]", self, arg1, arg2);
-    NSLog(@"asdfdsahljkadsghjklgdsajlhkdsafhjkldgsafjhkdsafklgjasdjkldsafhjklagdsjklhdsajhklsdfjklhdsafjk");
-    
-	id originalReturnOfMessage = _logos_orig$_ungrouped$BKSApplicationStateMonitor$bundleInfoValueForKey$PID$(self, _cmd, arg1, arg2);
-	
-	if(originalReturnOfMessage == nil){
-        originalReturnOfMessage = @"com.apple.SpringBoard";
-    }
-    
-	return originalReturnOfMessage;
-}
-
-
 static __attribute__((constructor)) void _logosLocalInit() {
-{Class _logos_class$_ungrouped$SBScreenShotter = objc_getClass("SBScreenShotter"); MSHookMessageEx(_logos_class$_ungrouped$SBScreenShotter, @selector(finishedWritingScreenshot:didFinishSavingWithError:context:), (IMP)&_logos_method$_ungrouped$SBScreenShotter$finishedWritingScreenshot$didFinishSavingWithError$context$, (IMP*)&_logos_orig$_ungrouped$SBScreenShotter$finishedWritingScreenshot$didFinishSavingWithError$context$);Class _logos_class$_ungrouped$BKSApplicationStateMonitor = objc_getClass("BKSApplicationStateMonitor"); MSHookMessageEx(_logos_class$_ungrouped$BKSApplicationStateMonitor, @selector(bundleInfoValueForKey:PID:), (IMP)&_logos_method$_ungrouped$BKSApplicationStateMonitor$bundleInfoValueForKey$PID$, (IMP*)&_logos_orig$_ungrouped$BKSApplicationStateMonitor$bundleInfoValueForKey$PID$);} }
-#line 175 "/Users/ethangillius/Developer/Cydia/ScreenshotShareSheet/ScreenshotShareSheet/ScreenshotShareSheet.xm"
+{Class _logos_class$_ungrouped$SBScreenShotter = objc_getClass("SBScreenShotter"); MSHookMessageEx(_logos_class$_ungrouped$SBScreenShotter, @selector(finishedWritingScreenshot:didFinishSavingWithError:context:), (IMP)&_logos_method$_ungrouped$SBScreenShotter$finishedWritingScreenshot$didFinishSavingWithError$context$, (IMP*)&_logos_orig$_ungrouped$SBScreenShotter$finishedWritingScreenshot$didFinishSavingWithError$context$);} }
+#line 163 "/Users/ethangillius/Developer/Cydia/ScreenshotShareSheet/ScreenshotShareSheet/ScreenshotShareSheet.xm"
--- ScreenshotShareSheet/ScreenshotShareSheet.xm
+++ ScreenshotShareSheet/ScreenshotShareSheet.xm
@@ -1,5 +1,4 @@
 #import <SpringBoard/SpringBoard.h>
-#import <BackBoardServices/BKSApplicationStateMonitor.h>
 #import "TTOpenInAppActivity.h"
 #import <AudioToolbox/AudioToolbox.h>
 
@@ -21,102 +20,109 @@ iOSOpenDev post-project creation from template requirements (remove these lines 
     //NSLog(@"ScreenshotShareSheet: A screenshot was taken.");
     //the user just took a screenshot. We should offer to do something with it.
     //most main code goes here, BEFORE %orig
-
     ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
     
-    // Enumerate just the photos and videos group by using ALAssetsGroupSavedPhotos.
-    [library enumerateGroupsWithTypes:ALAssetsGroupSavedPhotos usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
+    //set up the settings path and get some settings values
+    NSString *settingsPath = [NSString stringWithFormat:@"%@/Library/Preferences/%@", NSHomeDirectory(), @"com.ethansquared.ScreenshotShareSheet.plist"];
+    NSMutableDictionary *settings = [NSMutableDictionary dictionaryWithContentsOfFile:
+                     settingsPath];
+	NSNumber* enabledPreference = [settings objectForKey:@"enabled"];
+    NSNumber* openInPreference = [settings objectForKey:@"openinenabled"];
+    NSNumber* delayPreference = [settings objectForKey:@"delay"];
+    //create a settings plist for first-time users
+    if(settings == nil){
+        settings = [[NSMutableDictionary alloc] init];
+        enabledPreference = [NSNumber numberWithBool:YES];
+        openInPreference = [NSNumber numberWithBool:NO];
+        delayPreference = [NSNumber numberWithDouble:0.40];
+        [settings setObject:enabledPreference forKey:@"enabled"];
+        [settings setObject:openInPreference forKey:@"openinenabled"];
+        [settings setObject:delayPreference forKey:@"delay"];
+        BOOL result = [settings writeToFile:settingsPath atomically:YES];
+        if(result){
+            NSLog(@"The user didn't have a settings file yet. Generated one");
+        }else{
+            NSLog(@"The user didn't have a settings file yet. Generating FAILED");
+        }
+    }
+    
+    if([enabledPreference boolValue]){
+    
+        // Enumerate just the photos and videos group by using ALAssetsGroupSavedPhotos.
+        [library enumerateGroupsWithTypes:ALAssetsGroupSavedPhotos usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
         
-        // Within the group enumeration block, filter to enumerate just photos.
-        [group setAssetsFilter:[ALAssetsFilter allPhotos]];
+            // Within the group enumeration block, filter to enumerate just photos.
+            [group setAssetsFilter:[ALAssetsFilter allPhotos]];
         
-        // Chooses the photo at the last index
-        [group enumerateAssetsAtIndexes:[NSIndexSet indexSetWithIndex:[group numberOfAssets] - 1] options:0 usingBlock:^(ALAsset *alAsset, NSUInteger index, BOOL *innerStop) {
+            // Chooses the photo at the last index
+            [group enumerateAssetsAtIndexes:[NSIndexSet indexSetWithIndex:[group numberOfAssets] - 1] options:0 usingBlock:^(ALAsset *alAsset, NSUInteger index, BOOL *innerStop) {
             
-            // The end of the enumeration is signaled by asset == nil.
-            if (alAsset) {
-                UIWindow* topWindow = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-                topWindow.hidden = NO;
-                topWindow.windowLevel = UIWindowLevelAlert;
-                UIViewController *vc  = [[UIViewController alloc] init];
+                // The end of the enumeration is signaled by asset == nil.
+                if (alAsset) {
+                    UIWindow* topWindow = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+                    topWindow.hidden = NO;
+                    topWindow.windowLevel = UIWindowLevelAlert;
+                    UIViewController *vc  = [[UIViewController alloc] init];
                 
-                [topWindow setRootViewController:vc];
-                //[vc release];
+                    [topWindow setRootViewController:vc];
+                    //[vc release];
                 
-                ALAssetRepresentation *representation = [alAsset defaultRepresentation];
-                UIImage *latestPhoto = [UIImage imageWithCGImage:[representation fullScreenImage]];
+                    ALAssetRepresentation *representation = [alAsset defaultRepresentation];
+                    UIImage *latestPhoto = [UIImage imageWithCGImage:[representation fullScreenImage]];
                 
-                NSString *tempDir = NSTemporaryDirectory();
-                NSString *path = [tempDir stringByAppendingPathComponent:[representation filename]];
-                NSURL *fileURL = [NSURL fileURLWithPath:path];
+                    NSString *tempDir = NSTemporaryDirectory();
+                    NSString *path = [tempDir stringByAppendingPathComponent:[representation filename]];
+                    NSURL *fileURL = [NSURL fileURLWithPath:path];
                 
-                [UIImagePNGRepresentation(latestPhoto) writeToFile:path atomically:NO];
+                    [UIImagePNGRepresentation(latestPhoto) writeToFile:path atomically:NO];
                 
-                TTOpenInAppActivity *openInActivity = [[TTOpenInAppActivity alloc] initWithView:vc.view andRect:vc.view.bounds];
+                    TTOpenInAppActivity *openInActivity = [[TTOpenInAppActivity alloc] initWithView:vc.view andRect:vc.view.bounds];
                 
-                // Do something interesting with the AV asset.
-                NSArray *activityItems = @[ fileURL ];
-                UIActivityViewController *activityController =
-                [[UIActivityViewController alloc]
-                 initWithActivityItems:activityItems
-                 applicationActivities:@[ openInActivity ]];
-                activityController.excludedActivityTypes = @[UIActivityTypeSaveToCameraRoll,UIActivityTypeAssignToContact, UIActivityTypePrint, UIActivityTypeAirDrop ];
+                    // Do something interesting with the AV asset.
+                    NSArray *activityItems = @[ fileURL ];
+                    UIActivityViewController *activityController;
+                    if([openInPreference boolValue]){
+                        activityController = [[UIActivityViewController alloc] initWithActivityItems:activityItems applicationActivities:@[ openInActivity ]];
+                    }else{
+                        activityController = [[UIActivityViewController alloc] initWithActivityItems:activityItems applicationActivities:@[ ]];
+                    }
+                    activityController.excludedActivityTypes = @[UIActivityTypeSaveToCameraRoll, UIActivityTypePrint, UIActivityTypeAirDrop ];
                 
-                if(UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone){
-                    openInActivity.superViewController = activityController;
-                    [openInActivity release];
-                    [vc presentViewController:activityController animated:YES completion:NULL];
-                } else {
-                    UIPopoverController *activityPopoverController = [[UIPopoverController alloc] initWithContentViewController:activityController];
-                    openInActivity.superViewController = activityPopoverController;
-                    [openInActivity release];
-                    [activityPopoverController presentPopoverFromRect:CGRectMake(CGRectGetMidX(vc.view.bounds), 0, 1, 1) inView:vc.view permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
-                 }
+                    if(UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone){
+                        openInActivity.superViewController = activityController;
+                        [openInActivity release];
+                        [vc presentViewController:activityController animated:YES completion:NULL];
+                    } else {
+                        UIPopoverController *activityPopoverController = [[UIPopoverController alloc] initWithContentViewController:activityController];
+                        openInActivity.superViewController = activityPopoverController;
+                        [openInActivity release];
+                        [activityPopoverController presentPopoverFromRect:CGRectMake(CGRectGetMidX(vc.view.bounds), 0, 1, 1) inView:vc.view permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+                    }
                 
 
                 
-                activityController.completionHandler = ^(NSString *activityType, BOOL completed) {
-                    //AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
+                    activityController.completionHandler = ^(NSString *activityType, BOOL completed) {
+                        //AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
                     
-                    [[NSFileManager defaultManager] removeItemAtURL:fileURL error:NULL];
+                        [[NSFileManager defaultManager] removeItemAtURL:fileURL error:NULL];
                     
-                    [topWindow performSelector:@selector(setHidden:) withObject:self afterDelay:0.40f]; // delay is for the animation to complete.
-                    [topWindow release];
-                    [activityController release];
-                };
+                        [topWindow performSelector:@selector(setHidden:) withObject:self afterDelay:[delayPreference floatValue]]; // delay is for the animation to complete.
+                        [topWindow release];
+                        [activityController release];
+                    };
                 
-            }
+                }
+            }];
+        } failureBlock: ^(NSError *error) {
+            // Typically you should handle an error more gracefully than this.
+            // but errors don't happen because we're AWESOME, so ignore this stupid warning above.
+            NSLog(@"No groups");
         }];
-    } failureBlock: ^(NSError *error) {
-        // Typically you should handle an error more gracefully than this.
-        // but errors don't happen because we're AWESOME, so ignore this stupid warning above.
-        NSLog(@"No groups");
-    }];
     
-    [library release];
+        [library release];
     
-    /*
-    NSArray *activityItems = @[@"image here"];
-    UIActivityViewController *activityController =
-    [[UIActivityViewController alloc]
-     initWithActivityItems:activityItems
-     applicationActivities:nil];
+    }
     
-    UIWindow* topWindow = [[[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds] retain];
-   // topWindow.windowLevel = 1200.0f;
-    topWindow.hidden = NO;
-    UIViewController *vc  = [[UIViewController alloc] init];
-    [topWindow setRootViewController:vc];
-    [vc presentViewController:activityController animated:YES completion:NULL];
-    
-    activityController.completionHandler = ^(NSString *activityType, BOOL completed) {
-        [topWindow performSelector:@selector(setHidden:) withObject:self afterDelay:0.40f]; // delay is for the animation to complete.
-        //let's hope this works
-        [topWindow release];
-        [vc release];
-        [activityController release];
-    };
-     */
 
     %orig(screenshot,error,context);
     
